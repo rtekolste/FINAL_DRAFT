2a21accd6a53b0d792c8707df9e80c1144e2bc67
Arsalan Ahmed <35544234+arsalan2400@users.noreply.github.com>
2018-04-23 12:52:49 -0400

    Update InputDataApr23.py

--- InputDataApr23.py
+++ InputDataApr23.py
@@ -1,36 +1,73 @@
-POP_SIZE = 2000     # cohort population size
-SIM_LENGTH = 5   # length of simulation (years)
-ALPHA = 0.05        # significance level for calculating confidence intervals
-DELTA_T = 1         # years (length of time step, how frequently you look at the patient)
-DISCOUNT = 0.03
+
+#           Well    HTN     Stroke  Detect  No-Detect   Death
+#
+# Well       0        0       0     0           0         0
+# HTN        0        0       0     0           0         0
+# Detect     0        0       0     0           0         0
+# No-Detect  0        0       0     0           0         0
+# Death      0        0       0     0           0         0
+
+
+ALPHA = 0.05
+DISCOUNT_RATE = 0.03
+DELTA_T = 1
+POP_SIZE = 5000
+SIM_LENGTH = 5 #years
 
 # transition matrix
 TRANS_MATRIX = [
     [0.00,  0.00,   0.00,   0.00], #Well
     [0.00,  0.00,   0.00,   0.00], #HTN
     [0.00,  0.00,   0.00,   0.00], #Stroke
-    [0.00,  0.00,   0.00,   0.00], #Death
     [0.00,  0.00,   0.00,   0.00], #Detect
     [0.00,  0.00,   0.00,   0.00], #No Detect
+    [0.00,  0.00,   0.00,   0.00], #Death
     ]
 
-# anticoagulation relative risk in reducing stroke incidence and stroke death while in “Post-Stroke”
-RR_STROKE = 0.65
-# anticoagulation relative risk in increasing mortality due to bleeding is 1.05.
-RR_BLEEDING = 1.05
+# transition matrix
+TRANS_MATRIX_MGSO4 = [
+    [0.00,  0.00,   0.00,   0.00], #Well
+    [0.00,  0.00,   0.00,   0.00], #HTN
+    [0.00,  0.00,   0.00,   0.00], #Stroke
+    [0.00,  0.00,   0.00,   0.00], #Detect
+    [0.00,  0.00,   0.00,   0.00], #No Detect
+    [0.00,  0.00,   0.00,   0.00], #Death
+    ]
+
+# transition matrix
+TRANS_MATRIX_ANTICOAG = [
+    [0.00,  0.00,   0.00,   0.00], #Well
+    [0.00,  0.00,   0.00,   0.00], #HTN
+    [0.00,  0.00,   0.00,   0.00], #Stroke
+    [0.00,  0.00,   0.00,   0.00], #Detect
+    [0.00,  0.00,   0.00,   0.00], #No Detect
+    [0.00,  0.00,   0.00,   0.00], #Death
+    ]
+
+# annual cost of medications
+COST_MGSO4 = 16.50   #a drug
+COST_ANTICOAG = 4.50 # a normal bp med/ checkup?
+
+# cost of events
+COST_STROKE = 12780    #heart attk
+
+# annual cost
+HEALTH_COST = [
+    0,   # Well
+    0,       # HTN
+    0,       # Stroke
+    0,   # Detect
+    0,    # No Detect
+    0,    # Death
+    0        # Stroke Death
+]
 
 HEALTH_UTILITY = [
-    1,  # well
-    0.8865,  # stroke ONLY WHEN THE CYCLE LENGTH IS 1 YEAR
-    0.9,  # post-stroke
-    0  # dead
+    0,   # Well
+    0,       # HTN
+    0,       # Stroke
+    0,   # Detect
+    0,    # No Detect
+    0,    # Death
+    0        # Stroke Death
 ]
-
-HEALTH_COST = [
-    0,
-    5000,  # stroke
-    200,  # post-stroke /year
-    0
-]
-
-DETECT_COST = 50
