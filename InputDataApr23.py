9216d1c253194ecfe3645448370425522349485d
Arsalan Ahmed <35544234+arsalan2400@users.noreply.github.com>
2018-04-30 10:35:49 -0400

    Update InputDataApr23.py

--- InputDataApr23.py
+++ InputDataApr23.py
@@ -6,7 +6,6 @@
 # Eclampsia  0        0       0       0           0    
 # Death      0        0       0       0           0    
 
-
 ALPHA = 0.05
 DISCOUNT_RATE = 0.03
 DISCOUNT=.03
@@ -15,7 +14,7 @@ POP_SIZE = 2000
 SIM_LENGTH = 20 #weeks
 
 # transition matrix
-TRANS_MATRIX = [
+BASELINE = [
     [.80,  0.05,   0.05,   0.05,  0.05], #Well
     [0.05,  .80,   0.05,   0.05,  0.05], #HTN
     [0.05,  0.05,   .80,   0.05,  0.05], #SeverePE
@@ -24,7 +23,7 @@ TRANS_MATRIX = [
     ]
 
 # transition matrix
-TRANS_MATRIX_MGSO4 = [
+SUPPLIES_NO_TRAINING = [
     [1.00,  0.00,   0.00,   0.00,  0.00], #Well
     [0.00,  1.00,   0.00,   0.00,  0.00], #HTN
     [0.00,  0.00,   1.00,   0.00,  0.00], #SeverePE
@@ -33,7 +32,16 @@ TRANS_MATRIX_MGSO4 = [
     ]
 
 # transition matrix
-TRANS_MATRIX_ANTICOAG = [
+BETTER_TRAINING = [
+    [.80,  0.05,   0.05,   0.05,  0.05], #Well
+    [0.05,  .80,   0.05,   0.05,  0.05], #HTN
+    [0.05,  0.05,   .80,   0.05,  0.05], #SeverePE
+    [0.05,  0.05,   0.05,   0.8,  0.05], #DEclampsia
+    [0.00,  0.00,   0.00,   0.00,  1.00], #Death
+    ]
+
+# transition matrix
+BETTER_SUPPLIES_AND_TRAINING = [
     [.80,  0.05,   0.05,   0.05,  0.05], #Well
     [0.05,  .80,   0.05,   0.05,  0.05], #HTN
     [0.05,  0.05,   .80,   0.05,  0.05], #SeverePE
